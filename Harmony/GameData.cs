bb5538f7b9798a180f7817a4d6a0741009aebb18
NathanKell <nathankell@gmail.com>
2024-06-07 01:09:52 -0700

    Change some hull models

--- Harmony/GameData.cs
+++ Harmony/GameData.cs
@@ -14,22 +14,26 @@ namespace UADRealism
         [HarmonyPatch(nameof(GameData.PostProcessAll))]
         internal static void Postfix_PostProcessAll(GameData __instance)
         {
-            // HACK so I don't have to fix NAR stats
-            foreach (var kvp in __instance.shipTypes)
-            {
-                if (kvp.Key == "dd")
-                    kvp.Value.armorMin = 0f;
-                else
-                    kvp.Value.armorMin *= 0.5f;
+            // So I don't have to edit textassets
+            // (This avoids having to edit and rerelease NAR, for example)
+            FixForNewSystems(__instance);
 
-                if (kvp.Key != "ic")
-                    kvp.Value.speedMin -= 2f;
-            }
+            // Make actual changes to NAR
+            NARChanges(__instance);
 
+            Database.FillDatabase();
+        }
+
+        private static void FixForNewSystems(GameData __instance)
+        {
+            // Fix various stats issues
             foreach (var kvp in __instance.stats)
             {
+                // Remove engine weight from everything.
+                // We directly calculate SHP required instead.
                 kvp.Value.effectx.Remove("engine_weight");
 
+                // We'll actually sim the hull geometry instead
                 switch (kvp.Key)
                 {
                     case "beam":
@@ -39,6 +43,9 @@ namespace UADRealism
                         continue;
                 }
 
+                // Elsewhere, just halve the effect of the operating range stat (except
+                // the funnel-based one.
+                // TODO: change how funnels work.
                 // The cleaner version of this code causes a IL2Cpp unhollower compile issue
                 // so we're doing this the slow and stupid way.
                 if (kvp.Key == "smoke_exhaust")
@@ -52,6 +59,7 @@ namespace UADRealism
                 kvp.Value.effectx["operating_range"] = new Il2CppSystem.Collections.Generic.KeyValuePair<float, float>(k * 0.25f, v * 0.25f);
             }
 
+            // Add our own Freeboard stat
             var sdFB = new StatData();
             var sdBeam = __instance.stats["beam"];
 
@@ -84,8 +92,73 @@ namespace UADRealism
                 // Let's reget each time just in case.
                 kvp.Value.statsx.Add(__instance.stats["freeboard"], 0f);
             }
+        }
 
-            Database.FillDatabase();
+        private static void NARChanges(GameData __instance)
+        {
+            // Some shiptype stats are too restrictive
+            foreach (var kvp in __instance.shipTypes)
+            {
+                if (kvp.Key == "dd")
+                    kvp.Value.armorMin = 0f;
+                else
+                    kvp.Value.armorMin *= 0.5f;
+
+                if (kvp.Key != "ic")
+                    kvp.Value.speedMin -= 2f;
+            }
+
+            foreach (var kvp in __instance.parts)
+            {
+                var part = kvp.Value;
+                if (part.isHull)
+                {
+                    switch (part.name)
+                    {
+                        // ** Britain **
+                        // G3/N3 had transom sterns
+                        case "bc_4_britain_g3":
+                        case "bb_4_britain":
+                            part.model = "brooklyn_hull_c_wide";
+                            part.sectionsMin = 1;
+                            part.sectionsMax = 5;
+                            break;
+
+                        // Minotaur class had a forecastle but also
+                        // a transom stern. Not clear if "ca_6_british_largecruiser"
+                        // or "ca_6_british". Either way, there's no transom +
+                        // forecastle model.
+
+
+                        // ** USA **
+                        // Atlanta hull is the wrong shape.
+                        // But use Brooklyn A rather than Brooklyn/Wichita/Cleveland/Baltimore B
+                        case "ca_6_desmoines1":
+                        case "ca_6_desmoines2":
+                            part.model = "brooklyn_hull_a";
+                            part.sectionsMin = 5;
+                            part.sectionsMax = 8;
+                            part.paramx.Add("CA_Heavy_USA", new Il2CppSystem.Collections.Generic.List<string>());
+                            break;
+
+
+                        // CA was using Bismarck hull (!) and Cleveland
+                        // was using Atlanta hull.
+                        case "ca_5_usa":
+                        case "cl_6_cleveland":
+                            part.model = "brooklyn_hull_b";
+                            part.sectionsMin = 5;
+                            part.sectionsMax = 7;
+                            break;
+
+                        // No good solution for Farragut/Porter--
+                        // they should have forecastles but also transoms
+                        // "dd_5_nose_large_usa" -- Porter
+                        // "dd_5_akizuki_stern_flat" -- Farragut
+
+                    }
+                }
+            }
         }
     }
 }
