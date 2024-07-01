276bea76d9992f663cf8f8577426f183973a2a37
NathanKell <nathankell@gmail.com>
2024-06-30 18:29:16 -0700

    Only add starting techs in shared designer when year is 1890. Gets around starting designs being ineligible for campaign start.

---
+++ TweaksAndFixes/Harmony/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+using Il2Cpp;
+
+namespace TweaksAndFixes
+{
+    [HarmonyPatch(typeof(GameManager))]
+    internal class Patch_GameManager
+    {
+        private static bool _IsRefreshSharedDesign = false;
+        
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(GameManager.RefreshSharedDesign))]
+        internal static void Prefix_RefreshSharedDesign()
+        {
+            _IsRefreshSharedDesign = true;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(GameManager.RefreshSharedDesign))]
+        internal static void Postfix_RefreshSharedDesign()
+        {
+            _IsRefreshSharedDesign = false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(GameManager.GetTechYear))]
+        internal static bool Prefix_GetTechYear(TechnologyData t, ref int __result)
+        {
+            if (_IsRefreshSharedDesign && G.ui.sharedDesignYear == 1890 && !t.effects.ContainsKey("start"))
+            {
+                __result = 9999;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
