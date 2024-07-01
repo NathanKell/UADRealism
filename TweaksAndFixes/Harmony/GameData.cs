b430f9f67a15efb95e88a8b8d3115e002406fdfa
NathanKell <nathankell@gmail.com>
2024-06-30 23:27:10 -0700

    Fix some stock randpart typos

--- TweaksAndFixes/Harmony/GameData.cs
+++ TweaksAndFixes/Harmony/GameData.cs
@@ -14,6 +14,41 @@ namespace TweaksAndFixes
         [HarmonyPatch(nameof(GameData.PostProcessAll))]
         internal static void Postfix_PostProcessAll(GameData __instance)
         {
+            List<string> fixKeys = new List<string>();
+            foreach (var rp in __instance.randParts.Values)
+            {
+                foreach (var kvp in rp.paramx)
+                {
+                    switch(kvp.Key)
+                    {
+                        case "and":
+                        case "or":
+                        case "mount":
+                        case "!mount":
+                        case "delete_unmounted":
+                        case "delete_refit":
+                        case "scheme":
+                        case "tr_rand_mod":
+                            continue;
+                    }
+                    fixKeys.Add(kvp.Key);
+                }
+                foreach (var key in fixKeys)
+                {
+                    var newVal = key.Replace(")", string.Empty).Replace("(", string.Empty);
+                    rp.paramx.Remove(key);
+                    // we use and unless it doesn't exist and or does.
+                    if (!rp.paramx.TryGetValue("and", out var lst) && !rp.paramx.TryGetValue("or", out lst))
+                    {
+                        lst = new Il2CppSystem.Collections.Generic.List<string>();
+                        rp.paramx["and"] = lst;
+                    }
+                    lst.Add(newVal);
+                    Melon<TweaksAndFixes>.Logger.Msg($"Fixing Randpart {rp.name} with param {rp.param}, invalid key {key}. New value {newVal}");
+                }
+                fixKeys.Clear();
+            }
+
             // Run after other things have a chance to update GameData
             MelonCoroutines.Start(FillDatabase());
         }
