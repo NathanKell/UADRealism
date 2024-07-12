4fbb66bdd38df2e72a9afdb0cf2e2621753109e0
NathanKell <nathankell@gmail.com>
2024-07-12 00:16:12 -0700

    Fix both regular and refit randparts

--- TweaksAndFixes/Harmony/GameData.cs
+++ TweaksAndFixes/Harmony/GameData.cs
@@ -10,43 +10,52 @@ namespace TweaksAndFixes
     [HarmonyPatch(typeof(GameData))]
     internal class Patch_GameData
     {
+        private static readonly List<string> _FixKeys = new List<string>();
+        private static void FixRandPart(RandPart rp)
+        {
+            foreach (var kvp in rp.paramx)
+            {
+                switch (kvp.Key)
+                {
+                    case "and":
+                    case "or":
+                    case "mount":
+                    case "!mount":
+                    case "delete_unmounted":
+                    case "delete_refit":
+                    case "scheme":
+                    case "tr_rand_mod":
+                        return;
+                }
+                _FixKeys.Add(kvp.Key);
+            }
+            foreach (var key in _FixKeys)
+            {
+                var newVal = key.Replace(")", string.Empty).Replace("(", string.Empty);
+                rp.paramx.Remove(key);
+                // we use and unless it doesn't exist and or does.
+                if (!rp.paramx.TryGetValue("and", out var lst) && !rp.paramx.TryGetValue("or", out lst))
+                {
+                    lst = new Il2CppSystem.Collections.Generic.List<string>();
+                    rp.paramx["and"] = lst;
+                }
+                lst.Add(newVal);
+                Melon<TweaksAndFixes>.Logger.Msg($"Fixing Randpart {rp.name} with param {rp.param}, invalid key {key}. New value {newVal}");
+            }
+            _FixKeys.Clear();
+        }
         [HarmonyPostfix]
         [HarmonyPatch(nameof(GameData.PostProcessAll))]
         internal static void Postfix_PostProcessAll(GameData __instance)
         {
-            List<string> fixKeys = new List<string>();
+            
             foreach (var rp in __instance.randParts.Values)
             {
-                foreach (var kvp in rp.paramx)
-                {
-                    switch (kvp.Key)
-                    {
-                        case "and":
-                        case "or":
-                        case "mount":
-                        case "!mount":
-                        case "delete_unmounted":
-                        case "delete_refit":
-                        case "scheme":
-                        case "tr_rand_mod":
-                            continue;
-                    }
-                    fixKeys.Add(kvp.Key);
-                }
-                foreach (var key in fixKeys)
-                {
-                    var newVal = key.Replace(")", string.Empty).Replace("(", string.Empty);
-                    rp.paramx.Remove(key);
-                    // we use and unless it doesn't exist and or does.
-                    if (!rp.paramx.TryGetValue("and", out var lst) && !rp.paramx.TryGetValue("or", out lst))
-                    {
-                        lst = new Il2CppSystem.Collections.Generic.List<string>();
-                        rp.paramx["and"] = lst;
-                    }
-                    lst.Add(newVal);
-                    Melon<TweaksAndFixes>.Logger.Msg($"Fixing Randpart {rp.name} with param {rp.param}, invalid key {key}. New value {newVal}");
-                }
-                fixKeys.Clear();
+                FixRandPart(rp);
+            }
+            foreach (var rp in __instance.randPartsRefit.Values)
+            {
+                FixRandPart(rp);
             }
 
             foreach (var data in __instance.parts.Values)
