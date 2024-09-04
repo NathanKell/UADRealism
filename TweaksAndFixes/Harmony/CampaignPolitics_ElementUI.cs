3d7c14a4bb6efa19dc1969e57292b96ff24f00ba
NathanKell <nathankell@gmail.com>
2024-09-04 02:05:50 -0700

    Move relations icons over left in politics window

---
+++ TweaksAndFixes/Harmony/CampaignPolitics_ElementUI.cs
@@ -0,0 +1,34 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+using Il2Cpp;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace TweaksAndFixes
+{
+    [HarmonyPatch(typeof(CampaignPolitics_ElementUI))]
+    internal class Patch_CampaignPolitics_ElementUI
+    {
+        [HarmonyPatch(nameof(CampaignPolitics_ElementUI.Init))]
+        [HarmonyPostfix]
+        internal static void Postfix_Init(CampaignPolitics_ElementUI __instance)
+        {
+            var rt = __instance.RelationsRoot.GetComponent<RectTransform>();
+            MelonCoroutines.Start(FixAnchor(rt));
+        }
+
+        internal static System.Collections.IEnumerator FixAnchor(RectTransform rt)
+        {
+            // For some reason we have to wait 2 frames.
+            // Presumably the anchor is getting reset after
+            // Init, somewhere.
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+
+            if (rt == null)
+                yield break;
+            rt.anchorMin = new Vector2(-0.03f, 1f);
+        }
+    }
+}
