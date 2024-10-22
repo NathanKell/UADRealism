13234c61d1b039e2b59c33d1a9310603131ab788
NathanKell <nathankell@gmail.com>
2024-10-21 22:18:35 -0700

    Prefix campaign research UI to replace techgroup sprites

---
+++ TweaksAndFixes/Harmony/CampaignResearchWindow.cs
@@ -0,0 +1,19 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+using Il2Cpp;
+using System.Collections.Generic;
+
+namespace TweaksAndFixes
+{
+    [HarmonyPatch(typeof(CampaignResearchWindow))]
+    internal class Patch_CampaignResearchWindow
+    {
+        [HarmonyPatch(nameof(CampaignResearchWindow.Show))]
+        [HarmonyPrefix]
+        internal static void Prefix_Show()
+        {
+            SpriteDatabase.Instance.OverrideResources();
+        }
+    }
+}
